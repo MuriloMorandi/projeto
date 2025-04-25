cf7249f9a8c83a1cfce3ba814cc2c16f72476093
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2025-04-25 00:09:20 -0300

    wip(test): criando testes de usuarios

--- apis/crud-api/__test__/userRouter.test.ts
+++ apis/crud-api/__test__/userRouter.test.ts
@@ -11,9 +11,10 @@ import {
 	afterAll,
 	beforeEach,
 } from 'vitest';
-import { faker } from '@faker-js/faker';
+import { fa, faker } from '@faker-js/faker';
 import { nanoid } from 'nanoid';
 import { TRPCError } from '@trpc/server';
+import { eq } from 'drizzle-orm';
 
 type SelectUser = typeof usersTable.$inferSelect;
 
@@ -27,35 +28,96 @@ describe('userRouter', () => {
 	let initialDatabase: SelectUser[] = [];
 	Array.from({ length: 100 }).map((value, idx) => {
 		initialDatabase.push({
-			email: faker.internet.email(),
-			name: faker.person.fullName(),
 			id: nanoid(),
+			name: faker.person.fullName(),
+			email: faker.internet.email(),
+		});
+	});
+
+	describe('Validando o retorno da API', () => { 
+		const ctx = createMockContext() as unknown as Context;
+		
+		it('Cadastro', async () => {
+			const userInsert = {
+				email: faker.internet.email(),
+				name: faker.person.fullName(),
+			};
+			
+			const caller = appRouter.createCaller(ctx);
+			const result = await caller.user.create(userInsert);
+			expect(result).toHaveProperty('id');
+			expect(result).toHaveProperty('name');
+			expect(result).toHaveProperty('email');
+		});
+
+		it('Atualização', async () => {
+			const userInsert = {
+				email: faker.internet.email(),
+				name: faker.person.fullName(),
+			};
+			
+			const caller = appRouter.createCaller(ctx);
+			const result = await caller.user.create(userInsert);
+			expect(result).toHaveProperty('id');
+			expect(result).toHaveProperty('name');
+			expect(result).toHaveProperty('email');
+		});
+
+		it('List', async () => {
+			const caller = appRouter.createCaller(ctx);
+
+			const result = await caller.user.list();
+			expect(result.data).toStrictEqual([]);
+			expect(result.count).toEqual(0);
+		});
+
+		it('Busca por id', async () => {
+			const userInsert = initialDatabase[0];
+			await ctx.db.insert(usersTable).values(userInsert).execute();
+
+			const caller = appRouter.createCaller(ctx);
+			
+			const findUser =await caller.user.get({ id: userInsert.id });
+			
+			expect(findUser).toMatchObject(userInsert);
+			expect(findUser).toHaveProperty('id');
+			expect(findUser).toHaveProperty('name');
+			expect(findUser).toHaveProperty('email');
+		});
+
+		it('Busca por id (não localizado)', async () => {
+			const caller = appRouter.createCaller(ctx);
+			
+			await expect(
+    			caller.user.get({ id: initialDatabase[0].id })
+  			).rejects.toThrow('Usuário não localizado.');
+		});
+
+		it('Delete by id', async () => {
+			const userInsert = initialDatabase[0];
+			await ctx.db.insert(usersTable).values(userInsert).execute();
+
+			const caller = appRouter.createCaller(ctx);
+			
+			await caller.user.delete({ id: userInsert.id });
+			
+			const findUser = await ctx.db.query.usersTable.findFirst({
+				where: eq(usersTable.id, userInsert.id),
+			});
+			
+			expect(findUser).toBeUndefined();
 		});
 	});
 
 	describe('List', () => {
-		let isFirstTestDone = false;
-
+		const ctx = createMockContext() as unknown as Context;
+		
 		beforeEach(async () => {
-			if (!isFirstTestDone) return;
-
-			const ctx = createMockContext() as unknown as Context;
+			await ctx.db.delete(usersTable).execute();
 			await ctx.db.insert(usersTable).values(initialDatabase);
 		});
 
-		it('Valida retorno da API', async () => {
-			const ctx = createMockContext() as unknown as Context;
-			const caller = appRouter.createCaller(ctx);
-
-			const result = await caller.user.list();
-
-			expect(result.data).toStrictEqual([]);
-			expect(result.count).toEqual(0);
-			isFirstTestDone = true;
-		});
-
 		it('Deve retornar os dados cadastrados seguindo a ordenação', async () => {
-			const ctx = createMockContext() as unknown as Context;
 			const caller = appRouter.createCaller(ctx);
 
 			const result = await caller.user.list();
@@ -133,4 +195,6 @@ describe('userRouter', () => {
 			}
 		});
 	});
+
+
 });
